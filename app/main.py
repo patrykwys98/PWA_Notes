d3244e9c2e80f36b8f63b3d9572ee0f61126dd9a
patrykwys98 <patrykwys98@gmail.com>
2022-11-30 00:50:04 +0100

    adding origins

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 import os
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, note, notebook, user
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #! Routes
 app.include_router(note.router, tags=['Note'], prefix='/api/note')
